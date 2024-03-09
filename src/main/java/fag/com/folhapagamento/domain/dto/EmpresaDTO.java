59cd7a472ab9ce63646c90042027eaa5b2f718c5
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-08 21:47:00 -0300

    Criação do EmpresaDTO

---
+++ src/main/java/fag/com/folhapagamento/domain/dto/EmpresaDTO.java
@@ -0,0 +1,19 @@
+package fag.com.folhapagamento.domain.dto;
+
+public class EmpresaDTO {
+
+    private String cnpj;
+
+    private String razaoSocial;
+
+    private String nomeFantasia;
+
+    private String endereco;
+
+    private String regimeTributario;
+
+    private String inscricaoEstadual;
+
+    private String inscricaoMunicipal;
+
+}
