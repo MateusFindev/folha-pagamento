235f1b810f465ff748227da243d17be046277041
MateusFindev <finprogramador@gmail.com>
2024-03-08 21:38:05 -0300

    initial commit

---
+++ folhapagamento/src/test/java/fag/com/folhapagamento/FolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package fag.com.folhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
