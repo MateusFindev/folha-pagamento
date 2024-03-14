90eda9bc5ccf6e7c5227f33c7e4f30fae1d23900
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-14 20:49:38 -0300

    Criação do EmpresaDTO
    De novo

---
+++ src/main/java/fag/com/folhapagamento/core/dtos/EmpresaDTO.java
@@ -0,0 +1,75 @@
+package fag.com.folhapagamento.core.dtos;
+
+public class EmpresaDTO {
+
+    private String cnpj;
+
+    private String razaoSocial;
+
+    private String nomeFantasia;
+
+    private String endereco;
+
+    private String regimeTributario;
+
+    private String inscricaoEstadual;
+
+    private String inscricaoMunicipal;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getRegimeTributario() {
+        return regimeTributario;
+    }
+
+    public void setRegimeTributario(String regimeTributario) {
+        this.regimeTributario = regimeTributario;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getInscricaoMunicipal() {
+        return inscricaoMunicipal;
+    }
+
+    public void setInscricaoMunicipal(String inscricaoMunicipal) {
+        this.inscricaoMunicipal = inscricaoMunicipal;
+    }
+
+}
