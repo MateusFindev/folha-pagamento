f1b517a7771e393e7345bf00f8394c3965564a93
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-14 21:28:54 -0300

    Implementação do ContratoMapper

---
+++ src/main/java/fag/com/folhapagamento/core/mappers/ContratoMapper.java
@@ -0,0 +1,30 @@
+package fag.com.folhapagamento.core.mappers;
+
+import fag.com.folhapagamento.core.dtos.ContratoDTO;
+import fag.com.folhapagamento.core.entities.ContratoBO;
+
+public class ContratoMapper {
+
+    public static ContratoDTO toDTO(ContratoBO bo) {
+        ContratoDTO dto = new ContratoDTO();
+
+        dto.setTipo(bo.getTipo());
+        dto.setCargaHoraria(bo.getCargaHoraria());
+        dto.setTipoFiliacao(bo.getTipoFiliacao());
+        dto.setEmpresa(EmpresaMapper.toDTO(bo.getEmpresa()));
+
+        return dto;
+    }
+
+    public static ContratoBO toDTO(ContratoDTO dto) {
+        ContratoBO bo = new ContratoBO();
+
+        bo.setTipo(dto.getTipo());
+        bo.setCargaHoraria(dto.getCargaHoraria());
+        bo.setTipoFiliacao(dto.getTipoFiliacao());
+        bo.setEmpresa(EmpresaMapper.toBO(dto.getEmpresa()));
+
+        return bo;
+    }
+
+}
