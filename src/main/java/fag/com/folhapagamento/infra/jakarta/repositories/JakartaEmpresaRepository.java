b96dd04f7710a0986df933f3f8cb597bfefb4001
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-14 21:50:33 -0300

    Implementação de método para buscar uma JakartaEmpresa pelo CNPJ

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaEmpresaRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaEmpresaRepository.java
@@ -2,12 +2,14 @@ package fag.com.folhapagamento.infra.jakarta.repositories;
 
 import fag.com.folhapagamento.infra.jakarta.models.JakartaEmpresa;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JakartaEmpresaRepository extends SimpleJpaRepository<JakartaEmpresa, Long> {
+public class JakartaEmpresaRepository extends SimpleJpaRepository<JakartaEmpresa, String> {
 
     private final EntityManager em;
 
@@ -15,6 +17,17 @@ public class JakartaEmpresaRepository extends SimpleJpaRepository<JakartaEmpresa
     public JakartaEmpresaRepository(EntityManager em) {
         super(JakartaEmpresa.class, em);
         this.em = em;
+    }
+
+    public JakartaEmpresa findByCNPJ(String cnpj) {
+        TypedQuery<JakartaEmpresa> query = em.createQuery("SELECT e FROM JakartaEmpresa e WHERE e.cnpj = :cnpj", JakartaEmpresa.class)
+                .setParameter("cnpj", cnpj);
+
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
 }
