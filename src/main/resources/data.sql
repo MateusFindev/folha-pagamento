e2f0bc265edeab1aee34e1acbb59ea68e644675a
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-14 21:17:25 -0300

    Mockando empresa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO empresa (cnpj, razao_social, nome_fantasia, endereco, regime_tributario, inscricao_estadual, inscricao_municipal)
+VALUES ('12345678901234', 'Empresa Exemplo', 'Exemplo LTDA', 'Rua Exemplo, 123', 'Simples Nacional', '123456789', '987654321');
