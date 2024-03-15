eda633265b454f9d4a69e93177c1b5f205a1c9c5
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-14 21:17:46 -0300

    Exclusão de interface desnecessária

--- src/main/java/fag/com/folhapagamento/core/repositories/IEmpresaDataBaseRepository.java
+++
@@ -1,9 +0,0 @@
-package fag.com.folhapagamento.core.repositories;
-
-import fag.com.folhapagamento.core.entities.EmpresaBO;
-
-public interface IEmpresaDataBaseRepository {
-
-    EmpresaBO persist(EmpresaBO bo);
-
-}
