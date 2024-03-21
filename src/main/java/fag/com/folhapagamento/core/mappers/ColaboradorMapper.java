a2fa54ec4327cec5e1583f58fc44e4eb46fb434a
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 19:20:43 -0300

    Implementação do ColaboradorMapper

---
+++ src/main/java/fag/com/folhapagamento/core/mappers/ColaboradorMapper.java
@@ -0,0 +1,36 @@
+package fag.com.folhapagamento.core.mappers;
+
+import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
+import fag.com.folhapagamento.core.entities.ColaboradorBO;
+
+public class ColaboradorMapper {
+
+    public static ColaboradorDTO toDTO(ColaboradorBO bo) {
+        ColaboradorDTO dto = new ColaboradorDTO();
+
+        dto.setCpf(bo.getCpf());
+        dto.setRg(bo.getRg());
+        dto.setCnh(bo.getCnh());
+        dto.setNome(bo.getNome());
+        dto.setAdmissao(bo.getAdmissao());
+        dto.setContrato(ContratoMapper.toDTO(bo.getContrato()));
+        dto.setDependentes(bo.getDependentes());
+
+        return dto;
+    }
+
+    public static ColaboradorBO toBO(ColaboradorDTO dto) {
+        ColaboradorBO bo = new ColaboradorBO();
+
+        bo.setCpf(dto.getCpf());
+        bo.setRg(dto.getRg());
+        bo.setCnh(dto.getCnh());
+        bo.setNome(dto.getNome());
+        bo.setAdmissao(dto.getAdmissao());
+        bo.setContrato(ContratoMapper.toBO(dto.getContrato()));
+        bo.setDependentes(dto.getDependentes());
+
+        return bo;
+    }
+
+}
