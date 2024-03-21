2bfbcfb520b9f89756bc59aa77771523c4da04bf
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 20:40:17 -0300

    Mockango informações de um cargo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO empresa (cnpj, razao_social, nome_fantasia, endereco, regime_tributario, inscricao_estadual, inscricao_municipal)
 VALUES ('12345678901234', 'Empresa Exemplo', 'Exemplo LTDA', 'Rua Exemplo, 123', 'Simples Nacional', '123456789', '987654321');
+
+INSERT INTO cargo (nome, descricao, nivel, funcao, salario_base, ad_periculosidade, ad_insalubridade, ad_funcao, comissao)
+VALUES ('Cargo de Teste', 'Descricao do Cargo', 1, 'Carregar Sacaria', 2334, false, false, false, false);
