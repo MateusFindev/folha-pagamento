21da15da47ad831bf9bcff896c4ed0c815a3de82
MateusFindev <finprogramador@gmail.com>
2024-03-21 19:22:43 -0300

    Criação BeneficioDTO

---
+++ src/main/java/fag/com/folhapagamento/core/dtos/BeneficioDTO.java
@@ -0,0 +1,55 @@
+package fag.com.folhapagamento.core.dtos;
+
+import java.math.BigDecimal;
+
+public class BeneficioDTO {
+    private String nome;
+
+    private String codigo;
+
+    private String descricao;
+
+    private BigDecimal valorPadrao;
+
+    private boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValorPadrao() {
+        return valorPadrao;
+    }
+
+    public void setValorPadrao(BigDecimal valorPadrao) {
+        this.valorPadrao = valorPadrao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
