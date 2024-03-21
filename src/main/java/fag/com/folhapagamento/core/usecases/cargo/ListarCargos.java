e9c110340dd1c56811a3b3bc040069541b60a607
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 20:54:35 -0300

    Implementação do caso de uso ListarCargos

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/cargo/ListarCargos.java
@@ -0,0 +1,20 @@
+package fag.com.folhapagamento.core.usecases.cargo;
+
+import fag.com.folhapagamento.core.dtos.CargoDTO;
+import fag.com.folhapagamento.core.repositories.ICargoRepository;
+
+import java.util.List;
+
+public class ListarCargos {
+
+    private final ICargoRepository repository;
+
+    public ListarCargos(ICargoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<CargoDTO> execute(CargoDTO dto) {
+        return this.repository.listAll();
+    }
+
+}
