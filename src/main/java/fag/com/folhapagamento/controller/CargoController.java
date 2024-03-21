545f712e16a42cebf0291372b38db320609d7cfb
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 20:40:04 -0300

    Criação de rota para listar todos os cargos

---
+++ src/main/java/fag/com/folhapagamento/controller/CargoController.java
@@ -0,0 +1,34 @@
+package fag.com.folhapagamento.controller;
+
+import fag.com.folhapagamento.core.dtos.CargoDTO;
+import fag.com.folhapagamento.service.CargoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/cargos")
+@CrossOrigin
+public class CargoController {
+
+    private final CargoService service;
+
+    @Autowired
+    public CargoController(CargoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CargoDTO>> list() {
+        List<CargoDTO> cargos = this.service.list();
+
+        return new ResponseEntity<>(cargos, HttpStatus.OK);
+    }
+
+}
