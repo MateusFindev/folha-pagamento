80993f28d0e62f46cebd749abd01eb798889ac7a
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 20:39:46 -0300

    Implementação de um método para listar todos os cargos

--- src/main/java/fag/com/folhapagamento/service/CargoService.java
+++ src/main/java/fag/com/folhapagamento/service/CargoService.java
@@ -1,8 +1,14 @@
 package fag.com.folhapagamento.service;
 
+import fag.com.folhapagamento.core.dtos.CargoDTO;
+import fag.com.folhapagamento.core.mappers.CargoMapper;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaCargoMapper;
+import fag.com.folhapagamento.infra.jakarta.models.JakartaCargo;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaCargoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CargoService {
@@ -12,6 +18,16 @@ public class CargoService {
     @Autowired
     public CargoService(JakartaCargoRepository repository) {
         this.repository = repository;
+    }
+
+    public List<CargoDTO> list() {
+        List<JakartaCargo> cargos = this.repository.findAll();
+
+        if (cargos.isEmpty()) {
+            return null;
+        }
+
+        return cargos.stream().map(cargo -> CargoMapper.toDTO(JakartaCargoMapper.toDomain(cargo))).toList();
     }
 
 }
