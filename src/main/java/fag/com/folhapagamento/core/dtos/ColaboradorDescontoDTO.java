e962cd25d4b329207f15bb89b3c95f760206fc4d
MateusFindev <finprogramador@gmail.com>
2024-03-21 21:58:13 -0300

    Atualização ColaboradorDescontoDTO

--- src/main/java/fag/com/folhapagamento/core/dtos/ColaboradorDescontoDTO.java
+++ src/main/java/fag/com/folhapagamento/core/dtos/ColaboradorDescontoDTO.java
@@ -5,7 +5,39 @@ import java.math.BigDecimal;
 public class ColaboradorDescontoDTO {
 
     private ColaboradorDTO colaborador;
-    private DescontoDTO descontoDTO;
+    private DescontoDTO desconto;
     private BigDecimal valor;
+    private boolean ativo;
 
+    public ColaboradorDTO getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(ColaboradorDTO colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public DescontoDTO getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(DescontoDTO desconto) {
+        this.desconto = desconto;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 }
