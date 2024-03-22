943a289843cc1516374928c3a467f426dad3a19f
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:14:18 -0300

    Criação do DepartamentoDTO

---
+++ src/main/java/fag/com/folhapagamento/core/dtos/DepartamentoDTO.java
@@ -0,0 +1,45 @@
+package fag.com.folhapagamento.core.dtos;
+
+public class DepartamentoDTO {
+
+    private String nome;
+
+    private String descricao;
+
+    private boolean ativo;
+
+    private EmpresaDTO empresa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public EmpresaDTO getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(EmpresaDTO empresa) {
+        this.empresa = empresa;
+    }
+
+}
