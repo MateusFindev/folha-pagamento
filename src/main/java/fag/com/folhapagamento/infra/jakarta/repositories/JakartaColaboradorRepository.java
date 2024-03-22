feada204f08efa4924d80c91e40a3a3384df0507
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:26:30 -0300

    Alteração no JakartaColaboradorRepository para implementar o caso de uso ListarColaborador

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaColaboradorRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaColaboradorRepository.java
@@ -1,13 +1,19 @@
 package fag.com.folhapagamento.infra.jakarta.repositories;
 
+import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
+import fag.com.folhapagamento.core.mappers.ColaboradorMapper;
+import fag.com.folhapagamento.core.usecases.colaborador.ListarColaborador;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaColaboradorMapper;
 import fag.com.folhapagamento.infra.jakarta.models.JakartaColaborador;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class JakartaColaboradorRepository extends SimpleJpaRepository<JakartaColaborador, Long> {
+public class JakartaColaboradorRepository extends SimpleJpaRepository<JakartaColaborador, Long> implements ListarColaborador {
 
     private final EntityManager em;
 
@@ -15,6 +21,13 @@ public class JakartaColaboradorRepository extends SimpleJpaRepository<JakartaCol
     public JakartaColaboradorRepository(EntityManager em) {
         super(JakartaColaborador.class, em);
         this.em = em;
+    }
+
+    @Override
+    public List<ColaboradorDTO> listAll() {
+        List<JakartaColaborador> colaboradores = this.findAll();
+
+        return colaboradores.stream().map(colaborador -> ColaboradorMapper.toDTO(JakartaColaboradorMapper.toDomain(colaborador))).toList();
     }
 
 }
