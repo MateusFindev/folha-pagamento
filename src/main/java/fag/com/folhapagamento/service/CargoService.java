659b50dd83d79c09df7b45f6febc7bafce55199e
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:14:11 -0300

    Removendo imports inutilizados no CargoService

--- src/main/java/fag/com/folhapagamento/service/CargoService.java
+++ src/main/java/fag/com/folhapagamento/service/CargoService.java
@@ -1,11 +1,8 @@
 package fag.com.folhapagamento.service;
 
 import fag.com.folhapagamento.core.dtos.CargoDTO;
-import fag.com.folhapagamento.core.mappers.CargoMapper;
 import fag.com.folhapagamento.core.repositories.ICargoRepository;
 import fag.com.folhapagamento.core.usecases.cargo.ListarCargos;
-import fag.com.folhapagamento.infra.jakarta.mappers.JakartaCargoMapper;
-import fag.com.folhapagamento.infra.jakarta.models.JakartaCargo;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaCargoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
