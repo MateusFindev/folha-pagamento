0624a2a8ee8294c677db2d89e3a48d42f50f2787
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:26:56 -0300

    Alteração no ColaboradorService para implementar o caso de uso ListarColaborador

--- src/main/java/fag/com/folhapagamento/service/ColaboradorService.java
+++ src/main/java/fag/com/folhapagamento/service/ColaboradorService.java
@@ -1,17 +1,26 @@
 package fag.com.folhapagamento.service;
 
+import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
+import fag.com.folhapagamento.core.usecases.colaborador.ListarColaborador;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaColaboradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ColaboradorService {
+public class ColaboradorService implements ListarColaborador {
 
     private final JakartaColaboradorRepository repository;
 
     @Autowired
     public ColaboradorService(JakartaColaboradorRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public List<ColaboradorDTO> listAll() {
+        return this.repository.listAll();
     }
 
 }
