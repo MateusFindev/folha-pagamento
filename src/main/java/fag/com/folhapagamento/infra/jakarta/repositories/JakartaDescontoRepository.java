5fcc7ce8fa6b20209c6a077afcf51f9376d9a48e
MateusFindev <finprogramador@gmail.com>
2024-03-21 21:09:49 -0300

    Atualização JakartaDescontoRepository

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaDescontoRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaDescontoRepository.java
@@ -1,11 +1,21 @@
-//package fag.com.folhapagamento.infra.jakarta.repositories;
-//
-//import fag.com.folhapagamento.infra.jakarta.models.JakartaDesconto;
-//import jakarta.persistence.EntityManager;
-//import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-//
-//public class JakartaDescontoRepository extends SimpleJpaRepository<JakartaDesconto, int> {
-//
-//    private final EntityManager em;
-//
-//}
+package fag.com.folhapagamento.infra.jakarta.repositories;
+
+import fag.com.folhapagamento.infra.jakarta.models.JakartaContrato;
+import fag.com.folhapagamento.infra.jakarta.models.JakartaDesconto;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JakartaDescontoRepository extends SimpleJpaRepository<JakartaDesconto, Long> {
+
+    private final EntityManager em;
+
+    @Autowired
+    public JakartaDescontoRepository(EntityManager em) {
+        super(JakartaDesconto.class, em);
+        this.em = em;
+    }
+}
