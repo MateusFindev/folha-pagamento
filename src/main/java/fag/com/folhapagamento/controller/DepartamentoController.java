657cc3b835d26e4ee220fc2ac17977a37aa2d73a
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:15:39 -0300

    Implementação do DepartamentoController
    Criação de rota para listar todos os departamentos

---
+++ src/main/java/fag/com/folhapagamento/controller/DepartamentoController.java
@@ -0,0 +1,34 @@
+package fag.com.folhapagamento.controller;
+
+import fag.com.folhapagamento.core.dtos.DepartamentoDTO;
+import fag.com.folhapagamento.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/departamentos")
+@CrossOrigin
+public class DepartamentoController {
+
+    private final DepartamentoService service;
+
+    @Autowired
+    public DepartamentoController(DepartamentoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartamentoDTO>> list() {
+        List<DepartamentoDTO> cargos = this.service.listAll();
+
+        return new ResponseEntity<>(cargos, HttpStatus.OK);
+    }
+
+}
