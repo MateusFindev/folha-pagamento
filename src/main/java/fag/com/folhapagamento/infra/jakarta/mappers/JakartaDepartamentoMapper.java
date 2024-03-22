1c1b3016149a624d8c66cf3a85e6d593c13bec28
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-21 21:15:18 -0300

    Implementação do JakartaDepartamentoMapper

---
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/mappers/JakartaDepartamentoMapper.java
@@ -0,0 +1,29 @@
+package fag.com.folhapagamento.infra.jakarta.mappers;
+
+import fag.com.folhapagamento.core.entities.DepartamentoBO;
+import fag.com.folhapagamento.infra.jakarta.models.JakartaDepartamento;
+
+public class JakartaDepartamentoMapper {
+
+    public static DepartamentoBO toDomain(JakartaDepartamento entity) {
+        DepartamentoBO domain = new DepartamentoBO();
+
+        domain.setNome(entity.getNome());
+        domain.setDescricao(entity.getDescricao());
+        domain.setAtivo(entity.isAtivo());
+        domain.setEmpresa(JakartaEmpresaMapper.toDomain(entity.getEmpresa()));
+
+        return domain;
+    }
+
+    public static JakartaDepartamento toEntity(DepartamentoBO domain) {
+        JakartaDepartamento entity = new JakartaDepartamento();
+
+        entity.setNome(domain.getNome());
+        entity.setDescricao(domain.getDescricao());
+        entity.setAtivo(domain.isAtivo());
+        entity.setEmpresa(JakartaEmpresaMapper.toEntity(domain.getEmpresa()));
+
+        return entity;
+    }
+}
