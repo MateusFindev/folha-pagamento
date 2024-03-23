02f7b9f7742627632c95a9046f31f97941295b00
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-23 18:22:04 -0300

    Alteração no caso de uso ListarCargos para uma interface
    Agora ele extende a interface Listar

--- src/main/java/fag/com/folhapagamento/core/usecases/cargo/ListarCargos.java
+++ src/main/java/fag/com/folhapagamento/core/usecases/cargo/ListarCargos.java
@@ -1,20 +1,7 @@
 package fag.com.folhapagamento.core.usecases.cargo;
 
 import fag.com.folhapagamento.core.dtos.CargoDTO;
-import fag.com.folhapagamento.core.repositories.ICargoRepository;
+import fag.com.folhapagamento.core.usecases.Listar;
 
-import java.util.List;
-
-public class ListarCargos {
-
-    private final ICargoRepository repository;
-
-    public ListarCargos(ICargoRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<CargoDTO> execute() {
-        return this.repository.listAll();
-    }
-
+public interface ListarCargos extends Listar<CargoDTO> {
 }
