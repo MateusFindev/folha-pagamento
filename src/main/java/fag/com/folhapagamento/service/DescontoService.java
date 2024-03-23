075bc35c54ddb572358a2642c9c9949d3f35c2bd
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-22 23:07:17 -0300

    Implementação do caso de uso ListarDescontos no DescontoService

--- src/main/java/fag/com/folhapagamento/service/DescontoService.java
+++ src/main/java/fag/com/folhapagamento/service/DescontoService.java
@@ -1,14 +1,24 @@
 package fag.com.folhapagamento.service;
 
+import fag.com.folhapagamento.core.dtos.DescontoDTO;
+import fag.com.folhapagamento.core.usecases.desconto.ListarDescontos;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaDescontoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DescontoService {
+public class DescontoService implements ListarDescontos {
 
     private final JakartaDescontoRepository repository;
 
     @Autowired
     public DescontoService (JakartaDescontoRepository repository) {this.repository = repository;}
+
+    @Override
+    public List<DescontoDTO> listAll() {
+        return this.repository.listAll();
+    }
+
 }
