6b6f6058655e2b57071c22b1808c5f80563c7eb9
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-23 18:22:21 -0300

    Alteração no caso de uso ListarDepartamentos para uma interface
    Agora ele extende a interface Listar

--- src/main/java/fag/com/folhapagamento/core/usecases/departamento/ListarDepartamentos.java
+++ src/main/java/fag/com/folhapagamento/core/usecases/departamento/ListarDepartamentos.java
@@ -1,20 +1,7 @@
 package fag.com.folhapagamento.core.usecases.departamento;
 
 import fag.com.folhapagamento.core.dtos.DepartamentoDTO;
-import fag.com.folhapagamento.core.repositories.IDepartamentoRepository;
+import fag.com.folhapagamento.core.usecases.Listar;
 
-import java.util.List;
-
-public class ListarDepartamentos {
-
-    private final IDepartamentoRepository repository;
-
-    public ListarDepartamentos(IDepartamentoRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<DepartamentoDTO> execute() {
-        return this.repository.listAll();
-    }
-
+public interface ListarDepartamentos extends Listar<DepartamentoDTO> {
 }
