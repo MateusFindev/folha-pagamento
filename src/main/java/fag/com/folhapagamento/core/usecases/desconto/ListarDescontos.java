fa6efd8757b19946dca952db2a811a84e79673a0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-22 23:06:39 -0300

    Definição do caso de uso ListarDescontos

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/desconto/ListarDescontos.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.desconto;
+
+import fag.com.folhapagamento.core.dtos.DescontoDTO;
+import fag.com.folhapagamento.core.usecases.Listar;
+
+public interface ListarDescontos extends Listar<DescontoDTO> {
+}
