a10301dac4d338c95425768ed5f164a021a6d2e9
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-25 20:12:27 -0300

    Criação da interface ListarDependentes
    Extendendo a interface Listar

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/dependente/ListarDependentes.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.dependente;
+
+import fag.com.folhapagamento.core.dtos.DependenteDTO;
+import fag.com.folhapagamento.core.usecases.Listar;
+
+public interface ListarDependentes extends Listar<DependenteDTO> {
+}
