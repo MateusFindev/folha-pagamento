1ea5129397805cafdd1777b247010d736a1fe930
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-25 20:10:54 -0300

    Criação do DependenteBO

---
+++ src/main/java/fag/com/folhapagamento/core/entities/DependenteBO.java
@@ -0,0 +1,49 @@
+package fag.com.folhapagamento.core.entities;
+
+import fag.com.folhapagamento.core.enums.EnumParentesco;
+
+import java.util.Date;
+
+public class DependenteBO {
+
+    private String nome;
+
+    private Date nascimento;
+
+    private EnumParentesco parentesco;
+
+    private ColaboradorBO colaborador;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public EnumParentesco getParentesco() {
+        return parentesco;
+    }
+
+    public void setParentesco(EnumParentesco parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    public ColaboradorBO getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(ColaboradorBO colaborador) {
+        this.colaborador = colaborador;
+    }
+
+}
