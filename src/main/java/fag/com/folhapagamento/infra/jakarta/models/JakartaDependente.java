511d291678a6c9289abd58e1da4ef005a43181df
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-26 20:58:34 -0300

    Criação da entidade JakartaDependente

---
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/models/JakartaDependente.java
@@ -0,0 +1,34 @@
+package fag.com.folhapagamento.infra.jakarta.models;
+
+import fag.com.folhapagamento.core.enums.EnumParentesco;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departamento")
+public class JakartaDependente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "nascimento", nullable = false)
+    private Date nascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "parentesco", nullable = false)
+    private EnumParentesco parentesco;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_colaborador", nullable = false)
+    private JakartaColaborador colaborador;
+
+}
