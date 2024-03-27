287d7467823100ea5c73102199617f0190e42af3
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-03-26 21:48:36 -0300

    Criação de rota para listar todos os colaboradores

--- src/main/java/fag/com/folhapagamento/controller/ColaboradorController.java
+++ src/main/java/fag/com/folhapagamento/controller/ColaboradorController.java
@@ -1,10 +1,16 @@
 package fag.com.folhapagamento.controller;
 
+import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
 import fag.com.folhapagamento.service.ColaboradorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/colaboradores")
@@ -16,6 +22,13 @@ public class ColaboradorController {
     @Autowired
     public ColaboradorController(ColaboradorService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ColaboradorDTO>> list() {
+        List<ColaboradorDTO> colaboradores = this.service.listAll();
+
+        return new ResponseEntity<>(colaboradores, HttpStatus.OK);
     }
 
 }
