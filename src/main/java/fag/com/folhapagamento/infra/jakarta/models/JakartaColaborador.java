350d540d1161b748697097252d2ea8fe6ecfbdf5
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-28 23:15:07 -0300

    Adicionada propriedade email ao JakartaColaborador

--- src/main/java/fag/com/folhapagamento/infra/jakarta/models/JakartaColaborador.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/models/JakartaColaborador.java
@@ -30,6 +30,9 @@ public class JakartaColaborador {
     @Column(name = "nome", nullable = false)
     private String nome;
 
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "admissao", nullable = false)
     private Date admissao;
