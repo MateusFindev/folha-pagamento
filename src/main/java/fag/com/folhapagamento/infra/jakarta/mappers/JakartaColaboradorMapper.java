86ab5cb33d0a9151e6c0db642e83b6b5839cc5ce
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-28 23:15:29 -0300

    Mapeamento da propriedade email no JakartaColaboradorMapper

--- src/main/java/fag/com/folhapagamento/infra/jakarta/mappers/JakartaColaboradorMapper.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/mappers/JakartaColaboradorMapper.java
@@ -8,10 +8,12 @@ public class JakartaColaboradorMapper {
     public static ColaboradorBO toDomain(JakartaColaborador entity) {
         ColaboradorBO domain = new ColaboradorBO();
 
+        domain.setId(entity.getId());
         domain.setCpf(entity.getCpf());
         domain.setRg(entity.getRg());
         domain.setCnh(entity.getCnh());
         domain.setNome(entity.getNome());
+        domain.setEmail(entity.getEmail());
         domain.setAdmissao(entity.getAdmissao());
         domain.setContrato(JakartaContratoMapper.toDomain(entity.getContrato()));
         domain.setCargo(JakartaCargoMapper.toDomain(entity.getCargo()));
@@ -23,10 +25,12 @@ public class JakartaColaboradorMapper {
     public static JakartaColaborador toEntity(ColaboradorBO domain) {
         JakartaColaborador entity = new JakartaColaborador();
 
+        entity.setId(domain.getId());
         entity.setCpf(domain.getCpf());
         entity.setRg(domain.getRg());
         entity.setCnh(domain.getCnh());
         entity.setNome(domain.getNome());
+        entity.setEmail(domain.getEmail());
         entity.setAdmissao(domain.getAdmissao());
         entity.setContrato(JakartaContratoMapper.toEntity(domain.getContrato()));
         entity.setCargo(JakartaCargoMapper.toEntity(domain.getCargo()));
