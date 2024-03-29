ca7f487be99e992750e5caa882e07bb9bfacfce4
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-28 23:16:09 -0300

    Mock de informações

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,5 +10,71 @@ VALUES (1, 'Cargo de Teste', 'Descricao do Cargo', 1, 'Carregar Sacaria', 2334, 
 INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
 VALUES (1, 'CLT', 44, 'EMPREGADO', 1);
 
-INSERT INTO colaborador (id, cpf, rg, cnh, nome, admissao, id_contrato, id_cargo, genero)
-VALUES (1, '08640463913', '16487593', '342342', 'Mateus Pfeffer', TIMESTAMP '2020-06-14 00:00:00', 1, 1, 'MASCULINO');
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (2, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (3, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (4, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (5, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (6, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (7, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (8, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (9, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (10, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (11, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO contrato (id, tipo, carga_horaria, tipo_filiacao, id_empresa)
+VALUES (12, 'CLT', 44, 'EMPREGADO', 1);
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (1, '08640463913', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 1, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (2, '08640463912', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer2@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 2, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (3, '08640463911', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer3@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 3, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (4, '08640463914', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer4@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 4, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (5, '08640463915', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer5@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 5, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (6, '08640463916', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer6@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 6, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (7, '08640463917', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer7@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 7, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (8, '08640463918', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer8@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 8, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (9, '08640463919', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer9@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 9, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (10, '08640463910', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer10@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 10, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (11, '08640463923', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer11@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 11, 1, 'MASCULINO');
+
+INSERT INTO colaborador (id, cpf, rg, cnh, nome, email, admissao, id_contrato, id_cargo, genero)
+VALUES (12, '08640463933', '16487593', '342342', 'Mateus Pfeffer', 'mpfeffer12@minha.fag.edu.br', TIMESTAMP '2020-06-14 00:00:00', 12, 1, 'MASCULINO');
