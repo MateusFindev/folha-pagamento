0a5652be8d6d335538d42b8963aa2347f947272b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-28 23:15:19 -0300

    Mapeamento das propriedades id e email no ColaboradorMapper

--- src/main/java/fag/com/folhapagamento/core/mappers/ColaboradorMapper.java
+++ src/main/java/fag/com/folhapagamento/core/mappers/ColaboradorMapper.java
@@ -8,10 +8,12 @@ public class ColaboradorMapper {
     public static ColaboradorDTO toDTO(ColaboradorBO bo) {
         ColaboradorDTO dto = new ColaboradorDTO();
 
+        dto.setId(bo.getId());
         dto.setCpf(bo.getCpf());
         dto.setRg(bo.getRg());
         dto.setCnh(bo.getCnh());
         dto.setNome(bo.getNome());
+        dto.setEmail(bo.getEmail());
         dto.setAdmissao(bo.getAdmissao());
         dto.setContrato(ContratoMapper.toDTO(bo.getContrato()));
         dto.setCargo(CargoMapper.toDTO(bo.getCargo()));
@@ -23,10 +25,12 @@ public class ColaboradorMapper {
     public static ColaboradorBO toBO(ColaboradorDTO dto) {
         ColaboradorBO bo = new ColaboradorBO();
 
+        bo.setId(dto.getId());
         bo.setCpf(dto.getCpf());
         bo.setRg(dto.getRg());
         bo.setCnh(dto.getCnh());
         bo.setNome(dto.getNome());
+        bo.setEmail(dto.getEmail());
         bo.setAdmissao(dto.getAdmissao());
         bo.setContrato(ContratoMapper.toBO(dto.getContrato()));
         bo.setCargo(CargoMapper.toBO(dto.getCargo()));
