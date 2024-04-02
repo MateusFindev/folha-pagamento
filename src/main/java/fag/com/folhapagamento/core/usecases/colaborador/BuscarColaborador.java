7e357141c8fbba55cecfacd63dd05b139aef39e6
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-01 22:58:30 -0300

    Ajuste no BuscarColaborador
    Compatibilização com o ajuste da estrutura dos casos de uso

--- src/main/java/fag/com/folhapagamento/core/usecases/colaborador/BuscarColaborador.java
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/BuscarColaborador.java
@@ -1,7 +1,7 @@
 package fag.com.folhapagamento.core.usecases.colaborador;
 
 import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
-import fag.com.folhapagamento.core.usecases.desconto.Buscar;
+import fag.com.folhapagamento.core.usecases.Buscar;
 
 public interface BuscarColaborador extends Buscar<ColaboradorDTO> {
 }
