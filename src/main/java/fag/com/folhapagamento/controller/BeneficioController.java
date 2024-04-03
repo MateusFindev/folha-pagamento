f85c17d0fdd5a68e8f4264e02176c9e830c54385
MateusFindev <finprogramador@gmail.com>
2024-04-02 22:41:42 -0300

    Atualização BeneficioController

--- src/main/java/fag/com/folhapagamento/controller/BeneficioController.java
+++ src/main/java/fag/com/folhapagamento/controller/BeneficioController.java
@@ -1,10 +1,17 @@
 package fag.com.folhapagamento.controller;
 
+import fag.com.folhapagamento.core.dtos.BeneficioDTO;
+import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
 import fag.com.folhapagamento.service.BeneficioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/beneficios")
@@ -15,5 +22,12 @@ public class BeneficioController {
 
     @Autowired
     public BeneficioController(BeneficioService service) {this.service = service;}
+
+    @GetMapping
+    public ResponseEntity<List<BeneficioDTO>> list() {
+        List<BeneficioDTO> beneficios = this.service.listAll();
+
+        return new ResponseEntity<>(beneficios, HttpStatus.OK);
+    }
 
 }
