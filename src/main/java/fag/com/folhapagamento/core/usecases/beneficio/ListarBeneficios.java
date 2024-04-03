c1bee0d3ec8971917992c54cfef0af53db51ccad
MateusFindev <finprogramador@gmail.com>
2024-04-02 22:43:34 -0300

    Criação do usecase ListarBeneficio

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/beneficio/ListarBeneficios.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.beneficio;
+
+import fag.com.folhapagamento.core.dtos.BeneficioDTO;
+import fag.com.folhapagamento.core.usecases.Listar;
+
+public interface ListarBeneficios extends Listar<BeneficioDTO> {
+}
