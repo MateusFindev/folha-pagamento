bcaee1569c7977445e8a230fb12ca342878e1464
MateusFindev <finprogramador@gmail.com>
2024-04-02 22:41:49 -0300

    Atualização BeneficioService

--- src/main/java/fag/com/folhapagamento/service/BeneficioService.java
+++ src/main/java/fag/com/folhapagamento/service/BeneficioService.java
@@ -1,15 +1,22 @@
 package fag.com.folhapagamento.service;
 
-import fag.com.folhapagamento.infra.jakarta.models.JakartaBeneficio;
+import fag.com.folhapagamento.core.dtos.BeneficioDTO;
+import fag.com.folhapagamento.core.usecases.beneficio.ListarBeneficios;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaBeneficioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BeneficioService {
+public class BeneficioService implements ListarBeneficios {
     private final JakartaBeneficioRepository repository;
 
     @Autowired
     public BeneficioService (JakartaBeneficioRepository repository) {this.repository = repository;}
 
+    @Override
+    public List<BeneficioDTO> listAll() {
+        return this.repository.listAll();
+    }
 }
