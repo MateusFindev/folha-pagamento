53e14655f32eee366d41625fa87f8f17026d476f
MateusFindev <finprogramador@gmail.com>
2024-04-02 22:42:14 -0300

    Atualização JakartaBenenficioRepository

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaBeneficioRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaBeneficioRepository.java
@@ -1,13 +1,19 @@
 package fag.com.folhapagamento.infra.jakarta.repositories;
 
+import fag.com.folhapagamento.core.dtos.BeneficioDTO;
+import fag.com.folhapagamento.core.mappers.BeneficioMapper;
+import fag.com.folhapagamento.core.usecases.beneficio.ListarBeneficios;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaBeneficioMapper;
 import fag.com.folhapagamento.infra.jakarta.models.JakartaBeneficio;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class JakartaBeneficioRepository extends SimpleJpaRepository<JakartaBeneficio,Long> {
+public class JakartaBeneficioRepository extends SimpleJpaRepository<JakartaBeneficio,Long> implements ListarBeneficios {
 
     private final EntityManager em;
 
@@ -15,5 +21,12 @@ public class JakartaBeneficioRepository extends SimpleJpaRepository<JakartaBenef
     public JakartaBeneficioRepository(EntityManager em) {
         super(JakartaBeneficio.class, em);
         this.em = em;
+    }
+
+    @Override
+    public List<BeneficioDTO> listAll() {
+        List<JakartaBeneficio> beneficios = this.findAll();
+
+        return beneficios.stream().map(beneficio -> BeneficioMapper.toDTO(JakartaBeneficioMapper.toDomain(beneficio))).toList();
     }
 }
