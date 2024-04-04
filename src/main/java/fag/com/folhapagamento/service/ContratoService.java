fade610d931cbbf3635130b044ff36dd1670a840
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 20:10:53 -0300

    Implementação do caso de uso findByColaboradorId no ContratoService

--- src/main/java/fag/com/folhapagamento/service/ContratoService.java
+++ src/main/java/fag/com/folhapagamento/service/ContratoService.java
@@ -1,17 +1,29 @@
 package fag.com.folhapagamento.service;
 
+import fag.com.folhapagamento.core.dtos.ContratoDTO;
+import fag.com.folhapagamento.core.usecases.contrato.BuscarContrato;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaContratoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ContratoService {
+public class ContratoService implements BuscarContrato {
 
     private final JakartaContratoRepository repository;
 
     @Autowired
     public ContratoService(JakartaContratoRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public ContratoDTO customFindById(Long id) {
+        return this.repository.customFindById(id);
+    }
+
+    @Override
+    public ContratoDTO findByColaboradorId(Long id) {
+        return this.repository.findByColaboradorId(id);
     }
 
 }
