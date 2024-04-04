f6fd305e68f8058d6dd8e0a3f8de5bd42703e74d
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 19:41:56 -0300

    Definição do contrato listAllByColaboradorId na ListarColaboradorDesconto

--- src/main/java/fag/com/folhapagamento/core/usecases/colaborador/desconto/ListarColaboradorDesconto.java
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/desconto/ListarColaboradorDesconto.java
@@ -3,5 +3,10 @@ package fag.com.folhapagamento.core.usecases.colaborador.desconto;
 import fag.com.folhapagamento.core.dtos.ColaboradorDescontoDTO;
 import fag.com.folhapagamento.core.usecases.Listar;
 
+import java.util.List;
+
 public interface ListarColaboradorDesconto extends Listar<ColaboradorDescontoDTO> {
+
+    List<ColaboradorDescontoDTO> listAllByColaboradorId(Long id);
+
 }
