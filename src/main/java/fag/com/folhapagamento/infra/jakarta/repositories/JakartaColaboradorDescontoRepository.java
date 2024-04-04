2e8db7d41da1f1be5dd8191c57d1ae1531a8f2d9
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 19:42:11 -0300

    Implementação do caso de uso listAllByColaboradorId no JakartaColaboradorDescontoRepository

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaColaboradorDescontoRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaColaboradorDescontoRepository.java
@@ -6,6 +6,8 @@ import fag.com.folhapagamento.core.usecases.colaborador.desconto.ListarColaborad
 import fag.com.folhapagamento.infra.jakarta.mappers.JakartaColaboradorDescontoMapper;
 import fag.com.folhapagamento.infra.jakarta.models.JakartaColaboradorDesconto;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,18 @@ public class JakartaColaboradorDescontoRepository extends SimpleJpaRepository<Ja
         List<JakartaColaboradorDesconto> colaboradorDescontos = this.findAll();
 
         return colaboradorDescontos.stream().map(colaboradorDesconto -> ColaboradorDescontoMapper.toDTO(JakartaColaboradorDescontoMapper.toDomain(colaboradorDesconto))).toList();
+    }
+
+    @Override
+    public List<ColaboradorDescontoDTO> listAllByColaboradorId(Long id) {
+        TypedQuery<JakartaColaboradorDesconto> query = em.createQuery("SELECT e FROM JakartaColaboradorDesconto e WHERE e.colaborador.id = :id", JakartaColaboradorDesconto.class)
+                .setParameter("id", id);
+
+        try {
+            return query.getResultList().stream().map(colaboradorDesconto -> ColaboradorDescontoMapper.toDTO(JakartaColaboradorDescontoMapper.toDomain(colaboradorDesconto))).toList();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
 }
