c67eabb0a148e58247fe95d298d68524eccf8935
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 19:36:45 -0300

    Implementação do ColaboradorDescontoService
    Implementação do caso de uso ListarColaboradorDesconto

---
+++ src/main/java/fag/com/folhapagamento/service/ColaboradorDescontoService.java
@@ -0,0 +1,26 @@
+package fag.com.folhapagamento.service;
+
+import fag.com.folhapagamento.core.dtos.ColaboradorDescontoDTO;
+import fag.com.folhapagamento.core.usecases.colaborador.desconto.ListarColaboradorDesconto;
+import fag.com.folhapagamento.infra.jakarta.repositories.JakartaColaboradorDescontoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ColaboradorDescontoService implements ListarColaboradorDesconto {
+
+    private final JakartaColaboradorDescontoRepository repository;
+
+    @Autowired
+    public ColaboradorDescontoService(JakartaColaboradorDescontoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<ColaboradorDescontoDTO> listAll() {
+        return this.repository.listAll();
+    }
+
+}
