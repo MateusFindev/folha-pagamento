796b9eb5495bf0d8e01386374bca26e2b9f0e121
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 19:15:51 -0300

    Adicionada propriedades no FolhaPagamentoDTO

--- src/main/java/fag/com/folhapagamento/core/dtos/FolhaPagamentoDTO.java
+++ src/main/java/fag/com/folhapagamento/core/dtos/FolhaPagamentoDTO.java
@@ -1,4 +1,68 @@
 package fag.com.folhapagamento.core.dtos;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class FolhaPagamentoDTO {
+
+    ColaboradorDTO colaborador;
+
+    BigDecimal salarioBruto;
+
+    BigDecimal salarioLiquido;
+
+    List<ColaboradorDescontoDTO> descontos;
+
+    String mes;
+
+    Integer diasUteis;
+
+    public ColaboradorDTO getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(ColaboradorDTO colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public BigDecimal getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(BigDecimal salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+
+    public BigDecimal getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public void setSalarioLiquido(BigDecimal salarioLiquido) {
+        this.salarioLiquido = salarioLiquido;
+    }
+
+    public List<ColaboradorDescontoDTO> getDescontos() {
+        return descontos;
+    }
+
+    public void setDescontos(List<ColaboradorDescontoDTO> descontos) {
+        this.descontos = descontos;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public Integer getDiasUteis() {
+        return diasUteis;
+    }
+
+    public void setDiasUteis(Integer diasUteis) {
+        this.diasUteis = diasUteis;
+    }
+
 }
