d1b4167ce3008af5231393ffd4769ef00068f6a3
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-04-04 20:10:20 -0300

    Implementação do caso de uso findByColaboradorId no JakartaContratoRepository

--- src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaContratoRepository.java
+++ src/main/java/fag/com/folhapagamento/infra/jakarta/repositories/JakartaContratoRepository.java
@@ -1,13 +1,19 @@
 package fag.com.folhapagamento.infra.jakarta.repositories;
 
+import fag.com.folhapagamento.core.dtos.ContratoDTO;
+import fag.com.folhapagamento.core.mappers.ContratoMapper;
+import fag.com.folhapagamento.core.usecases.contrato.BuscarContrato;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaContratoMapper;
 import fag.com.folhapagamento.infra.jakarta.models.JakartaContrato;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JakartaContratoRepository extends SimpleJpaRepository<JakartaContrato, Long> {
+public class JakartaContratoRepository extends SimpleJpaRepository<JakartaContrato, Long> implements BuscarContrato {
 
     private final EntityManager em;
 
@@ -15,6 +21,23 @@ public class JakartaContratoRepository extends SimpleJpaRepository<JakartaContra
     public JakartaContratoRepository(EntityManager em) {
         super(JakartaContrato.class, em);
         this.em = em;
+    }
+
+    @Override
+    public ContratoDTO customFindById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ContratoDTO findByColaboradorId(Long id) {
+        TypedQuery<JakartaContrato> query = em.createQuery("SELECT e FROM JakartaContrato e WHERE e.colaborador.id = :id", JakartaContrato.class)
+                .setParameter("id", id);
+
+        try {
+            return ContratoMapper.toDTO(JakartaContratoMapper.toDomain(query.getSingleResult()));
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
 }
