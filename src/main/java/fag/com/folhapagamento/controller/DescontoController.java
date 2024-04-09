31a1f1461068a39138de71e4266f2ff8d20a4710
MateusFindev <finprogramador@gmail.com>
2024-04-09 19:09:59 -0300

    Controller e Service do Desconto

--- src/main/java/fag/com/folhapagamento/controller/DescontoController.java
+++ src/main/java/fag/com/folhapagamento/controller/DescontoController.java
@@ -1,10 +1,16 @@
 package fag.com.folhapagamento.controller;
 
+import fag.com.folhapagamento.core.dtos.DescontoDTO;
 import fag.com.folhapagamento.service.DescontoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/descontos")
@@ -13,6 +19,15 @@ public class DescontoController {
     private final DescontoService service;
 
     @Autowired
-    public DescontoController (DescontoService service) {this.service = service;}
+    public DescontoController(DescontoService service) {
+        this.service = service;
+    }
 
+
+    @GetMapping
+    public ResponseEntity<List<DescontoDTO>> list() {
+        List<DescontoDTO> descontos = this.service.listAll();
+
+        return new ResponseEntity<>(descontos, HttpStatus.OK);
+    }
 }
--- src/main/java/fag/com/folhapagamento/service/DescontoService.java
+++ src/main/java/fag/com/folhapagamento/service/DescontoService.java
@@ -1,7 +1,13 @@
 package fag.com.folhapagamento.service;
 
 import fag.com.folhapagamento.core.dtos.DescontoDTO;
+import fag.com.folhapagamento.core.mappers.BeneficioMapper;
+import fag.com.folhapagamento.core.mappers.DescontoMapper;
+import fag.com.folhapagamento.core.usecases.desconto.BuscarDescontos;
 import fag.com.folhapagamento.core.usecases.desconto.ListarDescontos;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaBeneficioMapper;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaDescontoMapper;
+import fag.com.folhapagamento.infra.jakarta.models.JakartaDesconto;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaDescontoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DescontoService implements ListarDescontos {
+public class DescontoService implements ListarDescontos, BuscarDescontos {
 
     private final JakartaDescontoRepository repository;
 
@@ -21,4 +27,14 @@ public class DescontoService implements ListarDescontos {
         return this.repository.listAll();
     }
 
+    @Override
+    public Object customFindById(Long id) {
+        JakartaDesconto desconto = this.repository.findById(id).orElse(null);
+
+        if (desconto == null) {
+            return null;
+        }
+
+        return DescontoMapper.toDTO(JakartaDescontoMapper.toDomain(desconto));
+    }
 }
