14f1d536110b8acc1d0515d15338d7be16b2a4e0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-11 19:35:42 -0300

    Adicionada as propriedades descontos e beneficios no ColaboradorBO

--- src/main/java/fag/com/folhapagamento/core/entities/ColaboradorBO.java
+++ src/main/java/fag/com/folhapagamento/core/entities/ColaboradorBO.java
@@ -28,6 +28,47 @@ public class ColaboradorBO {
 
     private List<DependenteBO> dependentes;
 
+    private List<ColaboradorBeneficioBO> beneficios;
+
+    private List<ColaboradorDescontoBO> descontos;
+
+    public void adicionarBeneficio(BeneficioBO beneficio) {
+        if (validaSeBeneficioNaoExiste(beneficio)) {
+            return;
+        }
+
+        // TODO
+    }
+
+    public void adicionarDesconto(DescontoBO desconto) {
+        if (validaSeDescontoNaoExiste(desconto)) {
+            return;
+        }
+
+        // TODO
+    }
+
+    private boolean validaSeBeneficioNaoExiste(BeneficioBO beneficio) {
+        return beneficios.stream()
+                .noneMatch(b -> b.getBeneficio().getCodigo().equals(beneficio.getCodigo()));
+    }
+
+    private boolean validaSeDescontoNaoExiste(DescontoBO desconto) {
+        return descontos.stream()
+                .noneMatch(b -> b.getDesconto().getCodigo().equals(desconto.getCodigo()));
+    }
+
+//    public void adicionarBeneficio(TipoBeneficio tipo) {
+//        if (tipo == TipoBeneficio.ADICIONAL_PERICULOSIDADE && cargo.isAdicionalPericulosidade()) {
+//            Beneficio beneficio = new Beneficio(tipo);
+//            // Adicionar o benefício ao colaborador
+//        } else if (tipo == TipoBeneficio.ADICIONAL_FUNCAO && cargo.isAdicionalFuncao()) {
+//            Beneficio beneficio = new Beneficio(tipo);
+//            // Adicionar o benefício ao colaborador
+//        }
+//        // Outros tipos de benefício e verificações
+//    }
+
     public Long getId() {
         return id;
     }
@@ -110,6 +151,22 @@ public class ColaboradorBO {
 
     public void setDependentes(List<DependenteBO> dependentes) {
         this.dependentes = dependentes;
+    }
+
+    public List<ColaboradorBeneficioBO> getBeneficios() {
+        return beneficios;
+    }
+
+    public void setBeneficios(List<ColaboradorBeneficioBO> beneficios) {
+        this.beneficios = beneficios;
+    }
+
+    public List<ColaboradorDescontoBO> getDescontos() {
+        return descontos;
+    }
+
+    public void setDescontos(List<ColaboradorDescontoBO> descontos) {
+        this.descontos = descontos;
     }
 
 }
