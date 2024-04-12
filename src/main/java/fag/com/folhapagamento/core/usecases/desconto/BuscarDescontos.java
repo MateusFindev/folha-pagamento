91f545b448c558375972e93037ac75f906ff707a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 00:11:32 -0300

    Ajuste na tipagem da interface BuscarDescontos

--- src/main/java/fag/com/folhapagamento/core/usecases/desconto/BuscarDescontos.java
+++ src/main/java/fag/com/folhapagamento/core/usecases/desconto/BuscarDescontos.java
@@ -3,6 +3,5 @@ package fag.com.folhapagamento.core.usecases.desconto;
 import fag.com.folhapagamento.core.dtos.DescontoDTO;
 import fag.com.folhapagamento.core.usecases.Buscar;
 
-public interface BuscarDescontos<T> extends Buscar<DescontoDTO> {
-
+public interface BuscarDescontos extends Buscar<DescontoDTO> {
 }
