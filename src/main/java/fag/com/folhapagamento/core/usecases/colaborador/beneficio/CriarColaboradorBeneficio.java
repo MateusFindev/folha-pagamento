960a60114a844fed92235dc540b524db09db9501
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:11 -0300

    Criação do usecase CriarColaboradorBeneficio

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/beneficio/CriarColaboradorBeneficio.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.colaborador.beneficio;
+
+import fag.com.folhapagamento.core.entities.ColaboradorBeneficioBO;
+import fag.com.folhapagamento.core.usecases.Criar;
+
+public interface CriarColaboradorBeneficio extends Criar<ColaboradorBeneficioBO> {
+}
