10da6689d54ab8f585235f07d4097ab7df278fac
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:24 -0300

    Criação do usecase AtualizarColaboradorDesconto

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/desconto/AtualizarColaboradorDesconto.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.colaborador.desconto;
+
+import fag.com.folhapagamento.core.entities.ColaboradorDescontoBO;
+import fag.com.folhapagamento.core.usecases.Atualizar;
+
+public interface AtualizarColaboradorDesconto extends Atualizar<Long, ColaboradorDescontoBO> {
+}
