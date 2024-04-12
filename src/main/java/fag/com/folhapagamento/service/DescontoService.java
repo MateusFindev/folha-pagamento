c650d8c13d2071a5a12e267c3a4bdbd64ab4f980
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 00:11:44 -0300

    Ajuste no tipo de retorno do método no DescontoService

--- src/main/java/fag/com/folhapagamento/service/DescontoService.java
+++ src/main/java/fag/com/folhapagamento/service/DescontoService.java
@@ -1,11 +1,9 @@
 package fag.com.folhapagamento.service;
 
 import fag.com.folhapagamento.core.dtos.DescontoDTO;
-import fag.com.folhapagamento.core.mappers.BeneficioMapper;
 import fag.com.folhapagamento.core.mappers.DescontoMapper;
 import fag.com.folhapagamento.core.usecases.desconto.BuscarDescontos;
 import fag.com.folhapagamento.core.usecases.desconto.ListarDescontos;
-import fag.com.folhapagamento.infra.jakarta.mappers.JakartaBeneficioMapper;
 import fag.com.folhapagamento.infra.jakarta.mappers.JakartaDescontoMapper;
 import fag.com.folhapagamento.infra.jakarta.models.JakartaDesconto;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaDescontoRepository;
@@ -28,7 +26,7 @@ public class DescontoService implements ListarDescontos, BuscarDescontos {
     }
 
     @Override
-    public Object customFindById(Long id) {
+    public DescontoDTO customFindById(Long id) {
         JakartaDesconto desconto = this.repository.findById(id).orElse(null);
 
         if (desconto == null) {
