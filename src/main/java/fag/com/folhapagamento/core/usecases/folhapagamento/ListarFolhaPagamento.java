9600729f3392228acfbb46618d6ebc60a66e01de
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:35 -0300

    Criação do usecase ListarFolhaPagamento

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/folhapagamento/ListarFolhaPagamento.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.folhapagamento;
+
+import fag.com.folhapagamento.core.dtos.FolhaPagamentoDTO;
+import fag.com.folhapagamento.core.usecases.Listar;
+
+public interface ListarFolhaPagamento extends Listar<FolhaPagamentoDTO> {
+}
