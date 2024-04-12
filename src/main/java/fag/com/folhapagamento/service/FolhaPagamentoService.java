1754823780ae71e6090f260bcfea91f1f3b2da67
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:28:15 -0300

    Implementação dos usecases no FolhaPagamentoService

--- src/main/java/fag/com/folhapagamento/service/FolhaPagamentoService.java
+++ src/main/java/fag/com/folhapagamento/service/FolhaPagamentoService.java
@@ -1,17 +1,50 @@
 package fag.com.folhapagamento.service;
 
+import fag.com.folhapagamento.core.dtos.FolhaPagamentoDTO;
+import fag.com.folhapagamento.core.entities.FolhaPagamentoBO;
+import fag.com.folhapagamento.core.mappers.FolhaPagamentoMapper;
+import fag.com.folhapagamento.core.usecases.folhapagamento.BuscarFolhaPagamento;
+import fag.com.folhapagamento.core.usecases.folhapagamento.ListarFolhaPagamento;
+import fag.com.folhapagamento.infra.jakarta.mappers.JakartaFolhaPagamentoMapper;
+import fag.com.folhapagamento.infra.jakarta.models.JakartaFolhaPagamento;
 import fag.com.folhapagamento.infra.jakarta.repositories.JakartaFolhaPagamentoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class FolhaPagamentoService {
+public class FolhaPagamentoService  implements ListarFolhaPagamento, BuscarFolhaPagamento {
 
     private final JakartaFolhaPagamentoRepository repository;
 
     @Autowired
     public FolhaPagamentoService(JakartaFolhaPagamentoRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public List<FolhaPagamentoDTO> listAll() {
+        return this.repository.listAll();
+    }
+
+    @Override
+    public FolhaPagamentoDTO customFindById(Long id) {
+        JakartaFolhaPagamento folha = this.repository.findById(id).orElse(null);
+
+        if (folha == null) {
+            return null;
+        }
+
+        return FolhaPagamentoMapper.toDTO(JakartaFolhaPagamentoMapper.toDomain(folha, true));
+    }
+
+    @Transactional
+    public FolhaPagamentoDTO create(FolhaPagamentoBO bo) {
+        FolhaPagamentoBO entity = this.repository.create(bo);
+
+        return FolhaPagamentoMapper.toDTO(entity);
     }
 
 }
