f018bb9b201c1ceb76cd4c30f1192dc20da0a3ec
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:29 -0300

    Criação do usecase AtualizarColaboradorBeneficio

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/beneficio/AtualizarColaboradorBeneficio.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.colaborador.beneficio;
+
+import fag.com.folhapagamento.core.entities.ColaboradorBeneficioBO;
+import fag.com.folhapagamento.core.usecases.Atualizar;
+
+public interface AtualizarColaboradorBeneficio extends Atualizar<Long, ColaboradorBeneficioBO> {
+}
