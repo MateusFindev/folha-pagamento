4b03dfbe97684a1d117ea34f985a9ef542b78fdd
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 00:02:08 -0300

    Criação de rotas para adicionar um benefício ou desconto

--- src/main/java/fag/com/folhapagamento/controller/ColaboradorController.java
+++ src/main/java/fag/com/folhapagamento/controller/ColaboradorController.java
@@ -1,6 +1,8 @@
 package fag.com.folhapagamento.controller;
 
+import fag.com.folhapagamento.core.dtos.BeneficioDTO;
 import fag.com.folhapagamento.core.dtos.ColaboradorDTO;
+import fag.com.folhapagamento.core.dtos.DescontoDTO;
 import fag.com.folhapagamento.service.ColaboradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,6 +33,20 @@ public class ColaboradorController {
     @GetMapping("{id}")
     public ResponseEntity<ColaboradorDTO> findById(@PathVariable Long id) {
         ColaboradorDTO colaborador = this.service.customFindById(id);
+
+        return new ResponseEntity<>(colaborador, HttpStatus.OK);
+    }
+
+    @PostMapping("{id}/beneficios")
+    public ResponseEntity<ColaboradorDTO> adicionarBeneficio(@PathVariable Long id, @RequestBody BeneficioDTO dto) {
+        ColaboradorDTO colaborador = this.service.adicionarBeneficio(id, dto);
+
+        return new ResponseEntity<>(colaborador, HttpStatus.OK);
+    }
+
+    @PostMapping("{id}/descontos")
+    public ResponseEntity<ColaboradorDTO> adicionarDesconto(@PathVariable Long id, @RequestBody DescontoDTO dto) {
+        ColaboradorDTO colaborador = this.service.adicionarDesconto(id, dto);
 
         return new ResponseEntity<>(colaborador, HttpStatus.OK);
     }
