3f9d9309fa661af6c72902e29691077dfa863d5a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:21 -0300

    Criação do usecase CriarFolhaPagamento

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/folhapagamento/CriarFolhaPagamento.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.folhapagamento;
+
+import fag.com.folhapagamento.core.entities.FolhaPagamentoBO;
+import fag.com.folhapagamento.core.usecases.Criar;
+
+public interface CriarFolhaPagamento extends Criar<FolhaPagamentoBO> {
+}
