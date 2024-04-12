41edc1474a07ff8bcb06c4f6a445d6074c1de532
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:08 -0300

    Criação do usecase CriarColaboradorDesconto

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/colaborador/desconto/CriarColaboradorDesconto.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.colaborador.desconto;
+
+import fag.com.folhapagamento.core.entities.ColaboradorDescontoBO;
+import fag.com.folhapagamento.core.usecases.Criar;
+
+public interface CriarColaboradorDesconto extends Criar<ColaboradorDescontoBO> {
+}
