3f9a677053ecea013e58a52c470aae190c95b78b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 02:18:15 -0300

    Criação do BeneficioNaoEncontrado

---
+++ src/main/java/fag/com/folhapagamento/core/exceptions/beneficio/BeneficioNaoEncontrado.java
@@ -0,0 +1,4 @@
+package fag.com.folhapagamento.core.exceptions.beneficio;
+
+public class BeneficioNaoEncontrado extends RuntimeException {
+}
--- src/main/java/fag/com/folhapagamento/core/exceptions/beneficio/BeneficioNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package fag.com.folhapagamento.core.exceptions.beneficio;
-
-public class BeneficioNotFoundException extends RuntimeException {
-}
