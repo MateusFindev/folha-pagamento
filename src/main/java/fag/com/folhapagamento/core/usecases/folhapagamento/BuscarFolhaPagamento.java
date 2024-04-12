65049d54aebe34e18a1d1805da88443d6f5bcca3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:18 -0300

    Criação do usecase BuscarFolhaPagamento

---
+++ src/main/java/fag/com/folhapagamento/core/usecases/folhapagamento/BuscarFolhaPagamento.java
@@ -0,0 +1,7 @@
+package fag.com.folhapagamento.core.usecases.folhapagamento;
+
+import fag.com.folhapagamento.core.dtos.FolhaPagamentoDTO;
+import fag.com.folhapagamento.core.usecases.Buscar;
+
+public interface BuscarFolhaPagamento extends Buscar<FolhaPagamentoDTO> {
+}
