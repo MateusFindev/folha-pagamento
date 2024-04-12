66b8ce7b9019fe0540479cc9e8f54a359db341a7
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 02:19:20 -0300

    Criação do ColaboradorNaoEncontrado

---
+++ src/main/java/fag/com/folhapagamento/core/exceptions/colaborador/ColaboradorNaoEncontrado.java
@@ -0,0 +1,4 @@
+package fag.com.folhapagamento.core.exceptions.colaborador;
+
+public class ColaboradorNaoEncontrado extends ColaboradorException {
+}
--- src/main/java/fag/com/folhapagamento/core/exceptions/colaborador/ColaboradorNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package fag.com.folhapagamento.core.exceptions.colaborador;
-
-public class ColaboradorNotFoundException extends RuntimeException {
-}
