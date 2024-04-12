f840a84af27002274b8c12a99cf59864605bb8b0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-12 20:26:45 -0300

    Criação da exceção ColaboradorBeneficioNaoEncontado

---
+++ src/main/java/fag/com/folhapagamento/core/exceptions/colaborador/beneficio/ColaboradorBeneficioNaoEncontado.java
@@ -0,0 +1,6 @@
+package fag.com.folhapagamento.core.exceptions.colaborador.beneficio;
+
+import fag.com.folhapagamento.core.exceptions.colaborador.ColaboradorException;
+
+public class ColaboradorBeneficioNaoEncontado extends ColaboradorException {
+}
